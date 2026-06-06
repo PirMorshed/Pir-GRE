379657160843b4720ad8da51a1f82a9645785bff
Pir Morshed <59572072+PirMorshed@users.noreply.github.com>
2026-06-06 16:55:48 +0330

    Initial commit

---
+++ domains/proj.sbs/gapp.js
@@ -0,0 +1,14 @@
+export default {
+    owner: {
+        user: "gappjust",
+        email: "mshberesh@gmail.com",
+    },
+    records: [
+        {
+            type: "CNAME",
+            record: "gapp.pages.dev.",
+            proxied: true,
+            ttl: 60,
+        },
+    ]
+}
